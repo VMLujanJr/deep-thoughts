b647192fcd880d434c1e9994bcc7302497469ac6
VMLujanJr <vmlujanjr@outlook.com>
2022-07-18 21:38:10 -0500

    implemented new routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,12 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login.js';
+import NoMatch from './pages/NoMatch.js';
+import SingleThought from './pages/SingleThought.js';
+import Profile from './pages/Profile.js';
+import Signup from './pages/Signup.js';
 
 const httpLink = createHttpLink({
   uri: '/graphql', // shortened this => http://localhost:3001/graphql; to accommodate different url paths
@@ -19,13 +25,37 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={ client }>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route 
+                path='/'
+                element={ <Home /> }
+              />
+              <Route
+                path='/login'
+                element={ <Login /> }
+              />
+              <Route
+                path='/signup'
+                element={ <Signup /> }
+              />
+              <Route
+                path='/profile'
+                element={ <Profile /> }
+              />
+              <Route
+                path='/thought'
+                element={ <SingleThought /> }
+              />
+            </Routes>
+            <Home />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
